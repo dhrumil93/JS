c755bb6d5d4903d9789f7992def9b2ffd99ec5b3
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-01 16:12:02 +0530

    basics of string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,9 @@
+const name = "Dhrumil";
+const favNumber = 93 ;
+
+// console.log(name + favNumber );
+
+console.log(`Hello my name is ${name} And my favourite Number is ${favNumber}.`);
+const gameName = new String('Dhrumil');
+console.log(gameName.charAt(0));
+console.log(gameName.indexOf('h'));
