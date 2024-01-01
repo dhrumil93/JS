5f597cda98313f1ca2c9d60d8e7e340c5a69a8c6
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-01 16:29:59 +0530

    numbers and maths

---
+++ 01_basics/06_number.js
@@ -0,0 +1,10 @@
+const Score = 400
+console.log(Score);
+
+const Balance =new Number(100);
+console.log(Balance);
+console.log(Balance.toString().length);
+console.log(Balance.toFixed(2));
+
+const otherNumber = 12.356
+console.log(otherNumber.toPrecision(2));
