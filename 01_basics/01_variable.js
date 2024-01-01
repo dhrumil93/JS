39c8141e206d0c178d3300f4eaa1e358a51a2767
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-01 10:50:59 +0530

    datatypes

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -2,11 +2,11 @@ const userID = 24634
 let userEmail = "dhrmuilpatel0093@gmail.com" 
 var password = "0093"
 userCity = "Ahmedabad"
+let userState ;
 
 // userID = 25465 not valid 
 userEmail = "dc@gmail.com"
 password = "jkl"
 userCity = "Kerala"
 
-console.log(userID);
-console.table([userEmail, userCity,password]);
+console.table([userID, userEmail, userCity, password, userState]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,11 @@
+let Name = "Dhrumil"
+let Age = 19
+let isSignedIn = true ;
+
+console.log(Name);
+console.log(typeof Age);
+console.log(typeof isSignedIn);
+console.log(typeof null);
+console.log(typeof undefined);
+
+// null is object while undefined is unique type of data same as string number boolean.
