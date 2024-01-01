0aa97deff896a285b3f99848d2216c17b5daf88a
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-01 11:05:41 +0530

    Conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,16 @@
+// let Age = 18
+let Score = "9021"
+
+console.log(typeof Score);
+
+let valueInNumber = Number(Score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//  "9021" => 9021
+//  "9021abh" => Nan 
+// true =>1 false=>0
+
+let isSignedIn = 1
+let booleanIsSignedIn = Boolean(isSignedIn);
+console.log(booleanIsSignedIn);
