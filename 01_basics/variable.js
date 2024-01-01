5cfcc2eb6c952c1f23728df6dfc93c586f5b7864
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-01 10:23:14 +0530

    varible starting

---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const userID = 24634
+let userEmail = "dhrmuilpatel0093@gmail.com" 
+var password = "0093"
+userCity = "Ahmedabad"
+
+// userID = 25465 not valid 
+userEmail = "dc@gmail.com"
+password = "jkl"
+userCity = "Kerala"
+
+console.log(userID);
+console.table([userEmail, userCity,password]);
