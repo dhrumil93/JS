3f9726b5a68222382c92c16711031fe544fa0581
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-02 20:59:14 +0530

    Dates

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -2,3 +2,7 @@ let myDate = new Date();
 console.log(myDate.toString());
 console.log(myDate.getTimezoneOffset());
 console.log(myDate.toLocaleString());
+
+
+let myCreatedDate = new Date(2024 , 0 , 4)
+console.log(myCreatedDate.toDateString());
