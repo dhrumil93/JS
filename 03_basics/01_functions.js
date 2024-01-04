b553a8a57a187d278a6401d1e1b1675a588f8f86
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-04 16:19:15 +0530

    Functions Basic

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,17 @@
+function addition(num1 , num2) {
+    console.log(num1 + num2);
+}
+addition(3,4)
+
+function addition1(num1, num2) {
+    let result = num1 + num2;
+    return result
+}
+const result= addition1(6,7)
+console.log(result);
+
+function addition2(num1, num2) {
+    return num1+num2
+}
+const result1= addition2(4,5)
+console.log(result1);
