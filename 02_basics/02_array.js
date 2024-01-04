860a7d342cc171a1a061576d58d3335bde61fdd4
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-04 10:48:33 +0530

    Flat

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,11 +1,15 @@
-const heros = ["thor","Wanda","Hulk"]
-const cricketers = ["Bumrah","Rohit","Surya"]
+const heros = ["thor", "Wanda", "Hulk"];
+const cricketers = ["Bumrah", "Rohit", "Surya"];
 
 // heros.push(cricketers);
 // console.log(heros);
 
-const all = heros.concat(cricketers)
+const all = heros.concat(cricketers);
 console.log(all);
 
-const n1 = [...cricketers, ...heros]
+const n1 = [...cricketers, ...heros];
 console.log(n1);
+
+const arr = [1,2,3,4,[5,6,[7,8,[9]]]]
+const real_arr = arr.flat(Infinity);
+console.log(real_arr);
