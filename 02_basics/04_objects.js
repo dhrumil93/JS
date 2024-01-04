f5e7e5762b09dd63d289daa8df8db5746e498735
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-04 15:26:03 +0530

    Objects in depth

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,22 @@
+const iguser = new Object()
+
+iguser.id ="93"
+iguser.name = "Dhrumil"
+
+// console.log(iguser);
+
+const bgmiUser = {
+    userID: 93133 ,
+    useName:{
+        Firstname: "Dhrumil",
+        Lastname: "Patel"
+    }
+}
+console.log(bgmiUser.useName.Firstname);
+
+const obj1 = { 1:"A" ,2:"b"}
+const obj2 = { 3:"C" ,4:"d"}
+
+// const obj3 = Object.assign({},obj1,obj2)
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
