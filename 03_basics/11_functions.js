ba6b5e52beb7bcd5b327546f6e872934cfb228f4
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-04 16:55:44 +0530

    Function in depth

---
+++ 03_basics/11_functions.js
@@ -0,0 +1,16 @@
+function calculatePrice(...num1) {
+    return num1
+}
+
+console.log(calculatePrice(200,400));
+
+const user= {
+    name: "Dhrumil" ,
+    id : 93
+}
+
+function objectHandling(anyObject) {
+    console.log(`name is ${anyObject.name} id is ${anyObject.id}`);
+    
+}
+objectHandling(user)
