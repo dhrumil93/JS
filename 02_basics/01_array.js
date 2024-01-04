0ebb8970c5e7a124d81fb0b5b002360da7bc9d22
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-04 10:13:46 +0530

    Push pop in array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -3,3 +3,11 @@ console.log(myArr[2]);
 
 const arr2 = new Array(1,2,3,4,5);
 console.log(arr2[2]);
+
+
+//  myArr.push(5)
+//  myArr.pop()
+//  myArr.unshift(1)
+ myArr.shift()
+ console.log(myArr);
+ 
