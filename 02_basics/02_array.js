00e461019fe4e8029543c1a019e9373f9b782b2d
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-04 10:56:53 +0530

    Conversion

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -13,3 +13,7 @@ console.log(n1);
 const arr = [1,2,3,4,[5,6,[7,8,[9]]]]
 const real_arr = arr.flat(Infinity);
 console.log(real_arr);
+
+console.log(Array.isArray("Dhrumil"));
+console.log(Array.from("Dhrumil"));
+console.log(Array.from({name : "Dhrumil"}));
