7901789228225982705141f319ffe1c062ba9775
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-04 15:51:17 +0530

    Intro to API and JSON

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -20,3 +20,12 @@ const obj2 = { 3:"C" ,4:"d"}
 // const obj3 = Object.assign({},obj1,obj2)
 const obj3 = {...obj1,...obj2}
 console.log(obj3);
+
+const course ={
+    courseName: "JS Tutorial",
+    price: "500",
+    courseInstructor: "MSP"
+}
+
+const {courseInstructor: Instructor} = course
+console.log(Instructor);
