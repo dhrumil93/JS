42113a5b2a5310db1b0ca831da1bf1f64596f93b
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-05 21:04:05 +0530

    Arrow Function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,15 @@
+const user ={
+    username: "Dhrumil",
+    age: 19,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} welcome to webPage`);
+        console.log(this); 
+    }
+}
+user.welcomeMessage();
+
+user.username= "Txnvi"
+user.welcomeMessage();
+
+console.log(this);
