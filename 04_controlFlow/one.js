b3fd553541cedbb2ff8a677e1e1aae6d7832e52a
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-06 15:20:17 +0530

    Example

--- 04_controlFlow/one.js
+++ 04_controlFlow/one.js
@@ -1,9 +1,19 @@
 // if
-const userLoggedIn = true;
-const temparature = 41;
+// const userLoggedIn = true;
+// const temparature = 41;
 
-if (temparature > 50) {
-  console.log("Condition is true");
-} else {
-  console.log("condition is false");
+// if (temparature > 50) {
+//   console.log("Condition is true");
+// } else {
+//   console.log("condition is false");
+// }
+
+const score = 99 ;
+
+if (score > 100) {
+    const power = "Kick";
+    console.log(`User Power is : ${power}`);
+} else{
+    const power = "Fly";
+    console.log(`User Power is : ${power}`);
 }
