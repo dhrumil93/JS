965cfd4687e8f0c704e793ecbe3cf2e79b674a18
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-07 16:12:41 +0530

    For Each

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,5 +1,14 @@
 const coding = ["JS","Python","Ruby","CPP","HTML" ]
 
 coding.forEach(function item(val) {
-    console.log(val);
+    // console.log(val);
 })
+
+coding.forEach( (val) => {
+    // console.log(val);
+} )
+
+function printMe(item) {
+    console.log(item);
+}
+coding.forEach(printMe)
