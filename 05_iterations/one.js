f5cb73c6116ad16def37c8311fd1d1aed4e530de
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-07 11:04:57 +0530

    Loop With Array

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -13,3 +13,11 @@ for (let i = 0; i < 10; i++) {
         
     }
 }
+
+
+let myArray = ["bumrah" ,"Kohli", "Rohit"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+    
+}
