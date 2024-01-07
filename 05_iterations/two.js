d8ffc2b72ef47a5020389031599d58eddc23460d
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-07 11:26:29 +0530

    Do While Loop

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -5,3 +5,13 @@ while (i <= 10) {
     console.log(`Value of i ${i}`);
     i = i+4;
 }
+
+// DO WHILE LOOP
+
+let a = 10 ;
+
+do {
+    console.log(`Value of ${a}`);
+    a++;
+    
+} while (a <= 15);
