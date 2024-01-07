e289c70a14c1b2f9da8a212c5247ac485235e7fa
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-07 20:23:39 +0530

    Filter and For each functions

---
+++ 05_iterations/six.js
@@ -0,0 +1,10 @@
+const coding = ["JS", "Python", "Ruby", "CPP", "HTML"];
+
+const values = coding.forEach((item) => {
+  console.log(item);
+});
+console.log(values);
+
+const myNum = [1,2,3,4,5,6,7,8,9,10]
+const newNum = myNum.filter( (num) => num > 4 )
+console.log(newNum);
