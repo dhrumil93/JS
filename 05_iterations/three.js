4e0d928daaef37f83c689f9fd4a6641b0373fb3f
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-07 12:15:24 +0530

    For Of Loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,13 @@
+//  For Of Loop
+
+ const arr = [1,2,3,4,5,6];
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "Hello World !"
+for (const greet of greetings) {
+    console.log(`Each charachter is ${greet}`);
+    
+}
