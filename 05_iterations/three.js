15f01e6579d6740ace844322a3e6cd4ab8661c2a
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-07 14:59:47 +0530

    Map Begining

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -11,3 +11,22 @@ for (const greet of greetings) {
     console.log(`Each charachter is ${greet}`);
     
 }
+
+const map = new Map();
+map.set('IN',"India");
+map.set('USA',"United States of America");
+map.set('UK',"United Kingdom");
+// console.log(map);
+
+for (const [key , value] of map) {
+    console.log(key, ':-', value);
+}
+
+
+// const games ={
+//     'game1' : "NFS",
+//     'game2' : "GTAV"
+// }
+// for (const game of games) {
+//     console.log(game);
+// }
