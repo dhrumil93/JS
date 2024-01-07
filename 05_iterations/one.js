d3d7fa66058298d43e46e13f22e35a0a15b299f1
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-07 10:50:06 +0530

    Loop In details

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -4,3 +4,12 @@ for (let i = 0; i < 10; i++) {
     const element = i;
     console.log(element);    
 }
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(`Outer Loop Element : ${i}`);    
+    for (let j = 0; j < 10; j++) {
+        console.log(`Inner Loop Element : ${j} And Outer Loop Element : ${i}`);
+        
+    }
+}
