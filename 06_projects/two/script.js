342ed66dd87d49dde35f269b8ca9af128d1c6ee0
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-13 11:15:05 +0530

    JS For BMI

---
+++ 06_projects/two/script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('.form');
+
+form.addEventListener('submit',function (event) {
+    event.preventDefault();
+    
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+
+})
