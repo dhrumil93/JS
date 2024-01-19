34eedbf0ab808164974959ee0629eb7872007781
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-19 22:47:22 +0530

    Final JS for Unlimited Color Change

--- 06_projects/six/script.js
+++ 06_projects/six/script.js
@@ -1,21 +1,24 @@
 const randomColor = function () {
-  const hex = "0123456789ABCDEF";
+  const hex = '0123456789ABCDEF';
   let color = '#';
   for (let i = 0; i < 6; i++) {
     color += hex[Math.floor(Math.random() * 16)];
   }
   return color;
 };
-
+let intervalID ;
 const startChangeColor = function () {
-
-    setInterval(changeColor,1000)
+    if (!intervalID) {
+        intervalID = setInterval(changeColor,1000);
+    }
     function changeColor() {
         document.body.style.backgroundColor = randomColor();
-        
     }
 };
-const stopChangeColor = function () {};
+const stopChangeColor = function () {
+    clearInterval (intervalID);
+    intervalID = null ;
+};
 
 document.querySelector('#start').addEventListener('click', startChangeColor);
 document.querySelector('#stop').addEventListener('click', stopChangeColor);
