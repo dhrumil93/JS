85986ad233f03e1d19cac0a9839affc124c0eb1c
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-21 18:07:18 +0530

    Another Syantax for promise

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -9,3 +9,12 @@ promiseD.then(function(){
     console.log("Promise Is Completed");
 }
 )
+
+new Promise(function(resolve , reject){
+    setTimeout(() => {
+        console.log("Task 2 is completed")
+        resolve();
+    }, 1000);
+}).then(function(){
+    console.log("Promise 2 Completed");
+})
