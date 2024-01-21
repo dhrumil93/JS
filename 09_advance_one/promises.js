fe0de320cf1a7b0f444156263cd45b7a1bbefc8f
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-21 18:25:32 +0530

    Another Method for Promise

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,20 +1,29 @@
-const promiseD = new Promise(function(resolve , reject){
-    setTimeout(() => {
-        console.log("Task Completed");
-        resolve()
-    }, 1000);
-})
+const promiseD = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("Task Completed");
+    resolve();
+  }, 1000);
+});
 
-promiseD.then(function(){
-    console.log("Promise Is Completed");
-}
-)
+promiseD.then(function () {
+  console.log("Promise Is Completed");
+});
 
-new Promise(function(resolve , reject){
+const promiseH = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    console.log("Task 2 is completed");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Promise 2 Completed");
+});
+
+const promiseR = new Promise(function (resolve, reject) {
     setTimeout(() => {
-        console.log("Task 2 is completed")
-        resolve();
+        resolve({userName : "Dhrumil" , email : "dhrumilpatel@gmail.com"});
     }, 1000);
-}).then(function(){
-    console.log("Promise 2 Completed");
+});
+
+promiseR.then(function(user){
+    console.log(user);
 })
