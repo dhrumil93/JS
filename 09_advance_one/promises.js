dc40267202e3c25777259df9301263ac8a089f26
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-23 20:57:29 +0530

    Fetch Without Try Catch Block

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -71,13 +71,22 @@ async function promiseMy() {
 
 promiseMy()
 
-async function getallUsers(){ 
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await response.json()
-    console.log(data);
-  } catch (error) {
-    console.log("E: ",error);
-  }
-}
-getallUsers()
+// async function getallUsers(){ 
+//   try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+//     console.log(data);
+//   } catch (error) {
+//     console.log("E: ",error);
+//   }
+// }
+// getallUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+  return response.json()
+})
+.then((data) => {
+  console.log(data);
+})
+.catch((error) => console.log(error))
