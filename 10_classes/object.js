8b98d5f9cbbf54eb2b33e6648e27d1a887e8a9de
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-26 20:38:50 +0530

    Prototypes in JS

--- 10_classes/object.js
+++ 10_classes/object.js
@@ -8,5 +8,18 @@ console.log(mul5(5))
 
 function user(userName , score) {
     this.userName = userName;
-    
+    this.score = score;
 }
+user.prototype.increment = function(){
+    this.score++
+}
+user.prototype.printMe = function(){
+    console.log(`Score is ${this.score}`);
+}
+
+const one = new user("dhrumil" , 23)
+const two = new user("Patel" , 24)
+
+
+one.printMe()
+console.log(one.increment());
