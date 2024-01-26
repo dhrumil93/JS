b3ca3a2e6bf494d9bb8c16ea656ddf927f19ee6a
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-26 19:37:48 +0530

    Prototypes in JS

--- 10_classes/object.js
+++ 10_classes/object.js
@@ -3,5 +3,10 @@ function mul5 (num){
 }
 
 console.log(mul5(5))
-console.log(mul5.power)
-console.log(mul5.prototype)
+// console.log(mul5.power)
+// console.log(mul5.prototype)
+
+function user(userName , score) {
+    this.userName = userName;
+    
+}
