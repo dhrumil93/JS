10a583aad7820c5717cdd1948506735099f4ddb1
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-26 15:54:33 +0530

    Without NEW keyword

--- 10_classes/oop.js
+++ 10_classes/oop.js
@@ -1,13 +1,25 @@
-const user = {
-    userName : "Dhrumil",
-    loginCounter : 8 ,
-    isloggedIn : true,
+// const user = {
+//     userName : "Dhrumil",
+//     loginCounter : 8 ,
+//     isloggedIn : true,
     
-    getallUser : function () {
-        console.log("User Details Fetch");
-        console.log(`Username : ${this.userName}`);
-    }
+//     getallUser : function () {
+//         console.log("User Details Fetch");
+//         console.log(`Username : ${this.userName}`);
+//     }
+// }
+
+// console.log(user.userName);
+// console.log(user.getallUser());
+
+function User(userName , loginCount , isloggedIn) {
+    this.userName = userName ;
+    this.loginCount = loginCount;
+    this.isloggedIn = isloggedIn;
+
+    return this;
 }
 
-console.log(user.userName);
-console.log(user.getallUser());
+const userOne = User("Dhrumil" , 12 , true)
+const userTwo = User("Patel" , 12 , true)
+console.log(userOne);
