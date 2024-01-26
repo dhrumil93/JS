e61a514694814a7fd30ce8b37966b9f84546106e
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-26 21:25:41 +0530

    Prototypes in JS

--- 10_classes/prototype.js
+++ 10_classes/prototype.js
@@ -1,38 +1,35 @@
-// 
-
-
+//
 
 // console.log(myName.trueLength);
 
-let myPlayers = ["bumrah" , "bhuvi"]
+let myPlayers = ["bumrah", "bhuvi"];
 
 let playersSpcl = {
-    bumrah: "Yorker",
-    bhuvi : "Swing",
+  bumrah: "Yorker",
+  bhuvi: "Swing",
 
-    getbhuviSpcl : function(){
-        console.log(`Bhuvi Speciality is ${this.bhuvi}`);
-    }
-}
+  getbhuviSpcl: function () {
+    console.log(`Bhuvi Speciality is ${this.bhuvi}`);
+  },
+};
 
-Array.prototype.dhrumilPatel = function(){
-    console.log("Dhrumil Is All-rounder");
-}
-Object.prototype.dhrumil = function(){
-    console.log("Dhrumil Is All-rounder");
-}
+Array.prototype.dhrumilPatel = function () {
+  console.log("Dhrumil Is All-rounder");
+};
+Object.prototype.dhrumil = function () {
+  console.log("Dhrumil Is All-rounder");
+};
 
 // myPlayers.dhrumilPatel()
 // myPlayers.dhrumil()
 // playersSpcl.dhrumilPatel()
 // playersSpcl.dhrumil()
 
-
 let myName = "dhrumil      ";
 let myName1 = "dhrumil1      ";
-String.prototype.trueLength = function(){
-    console.log(`${this}`);
-    console.log(`True Length is : ${this.trim().length}`);
-}
+String.prototype.trueLength = function () {
+  console.log(`${this}`);
+  console.log(`True Length is : ${this.trim().length}`);
+};
 myName.trueLength();
 myName1.trueLength();
