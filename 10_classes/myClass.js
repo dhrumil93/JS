4dfcbeeaff6bc1473552c8c1b36117f1bc04e931
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-01-27 10:58:05 +0530

    Same Code without Class

--- 10_classes/myClass.js
+++ 10_classes/myClass.js
@@ -1,18 +1,33 @@
-class user {
-    constructor (userName , email ,password){
+// class user {
+//     constructor (userName , email ,password){
+//         this.userName = userName;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+//     encryptPassword (){
+//         return `${this.password}dhru`
+//     }
+//     changeUserName(){
+//         return `${this.userName.toUpperCase()}`
+//     }
+// }
+
+// const dhrumil = new user("Dhrumil" , "example@gmail.com" , 123)
+// console.log(dhrumil.encryptPassword());
+// console.log(dhrumil.changeUserName());
+
+// Behind the scene
+
+function user1(userName , email ,password) {
         this.userName = userName;
         this.email = email;
         this.password = password;
-    }
-
-    encryptPassword (){
-        return `${this.password}dhru`
-    }
-    changeUserName(){
-        return `${this.userName.toUpperCase()}`
-    }
 }
 
-const dhrumil = new user("Dhrumil" , "example@gmail.com" , 123)
-console.log(dhrumil.encryptPassword());
-console.log(dhrumil.changeUserName());
+user1.prototype.userNamechange = function(){
+    return `${this.userName.toUpperCase()}`
+}
+
+const patel = new user1("Patel","dhrumil@gmail.com",153)
+console.log(patel.userNamechange());
