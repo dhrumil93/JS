26aee63484e15263f2ccf029f609011bd7f88834
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-02-03 11:33:44 +0530

    JS

--- practice/script.js
+++ practice/script.js
@@ -1,8 +1,10 @@
-function onclick() {
-    
-}
-const inputUser = {
-    userName : document.getElementById('name'),
-    passWord : document.getElementById('password')
-}
-console.log(inputUser);
+const formOne = document.getElementById('form');
+formOne.addEventListener('submit',function (event){
+    event.preventDefault();
+
+    const userName = (document.getElementById('name').value);
+    const passWord = (document.getElementById('password').value);
+
+    console.log(userName)
+    console.log(passWord);
+})
