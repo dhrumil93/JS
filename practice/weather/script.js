d0d36354009f37235b4424939b28de1b37dd6319
dhrumil93 <dhrumilpatel0093@gmail.com>
2024-02-17 18:43:54 +0530

    weather app

--- practice/weather/script.js
+++ practice/weather/script.js
@@ -1,7 +1,27 @@
-function getWeather() {
+function fetchWeather() {
   const city = document.getElementById("city").value;
-  const apiKey = "0f75e918918cf40a1dba6ce0c1fd84a2"; // Replace with your OpenWeatherMap API key
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const apiKey = "7383f1df1a40e18dee9bd84dafb10875"; // Replace with your OpenWeatherMap API key
+  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // Assuming the first result is the desired location
+      const location = data[0];
+      const lat = location.lat;
+      const lon = location.lon;
+      getWeather(lat, lon, city);
+    })
+    .catch((error) => {
+      console.error("Error fetching location data:", error);
+      const weatherDiv = document.getElementById("weather");
+      weatherDiv.innerHTML = "Error fetching location data. Please try again.";
+    });
+}
+
+function getWeather(lat, lon, city) {
+  const apiKey = "7383f1df1a40e18dee9bd84dafb10875"; // Replace with your OpenWeatherMap API key
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
   fetch(url)
     .then((response) => response.json())
